a5b7a3102e355aa4afd355a78e4332cba7cb1a44
Edgar Nunez <nure.edgare@gmail.com>
2023-03-21 02:18:31 -0700

    Reservation of Buy Tickets
    Using index of the alphabet list to locate rservations

--- src/seating.py
+++ src/seating.py
@@ -5,7 +5,6 @@ available_seat = '.'
 occupied_seat = 'X'
 empty_covid = 'e'
 
-
 seating = []
 n_row = 20
 n_col = 26
@@ -13,19 +12,28 @@ n_col = 26
 
 def alpha():
 
+    abc_list = []
+
     start = ord('a')
     end = ord('z')
+
     print()
     print('    ', end='\t')
+
     for i in range(start, end+1):
+
         ch = chr(i)
+        abc_list.append(ch)
         print(ch, end=' ')
 
     print()
     print('        '+('-')*51)
 
+    return abc_list
 
 # create some available seating
+
+
 def create_seating():
 
     for r in range(n_row):
@@ -41,25 +49,30 @@ def create_seating():
 def print_seating():
 
     for r in range(n_row):
-
         print(r+1, end="\t")
         for c in range(n_col):
-            seats_data = seating[r][c]
-            print(seats_data, end=" ")
+            print(seating[r][c], end=' ')
         print()
 
 
 # reservation seats
-def buy_ticket(fila, colum):
 
-    fila = int(input('Que fila es bro?'))
-    colum = int(input('Que columa es bro?'))
-    (seating[fila-1][colum-1]) = occupied_seat
 
+def buy_ticket():
+
+    row = int(input('Que fila es bro?'))
+
+    col_letter = input('Which Column?').lower()
+
+    col = alpha().index(col_letter)
+
+    (seating[row-1][col]) = occupied_seat
+
+    alpha()
     print_seating()
 
 
 create_seating()
 alpha()
 print_seating()
-buy_ticket(int, int)
+buy_ticket()
